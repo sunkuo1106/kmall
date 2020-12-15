5e2cc600cfe5ba21125e0084497ec43b8598eb29
sunkuo1106 <sunkuo1106@163.com>
2020-12-15 17:15:39 +0800

    增加dubbo并测试成功

--- kmall-user-service/src/main/java/com/kgc/kmall/user/service/MemberServiceImpl.java
+++ kmall-user-service/src/main/java/com/kgc/kmall/user/service/MemberServiceImpl.java
@@ -3,13 +3,14 @@ package com.kgc.kmall.user.service;
 import com.kgc.kmall.bean.Member;
 import com.kgc.kmall.service.MemberService;
 import com.kgc.kmall.user.mapper.MemberMapper;
+import org.apache.dubbo.config.annotation.Service;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @Component
-//dubbo @service
+@Service
 public class MemberServiceImpl implements MemberService {
     @Resource
     MemberMapper memberMapper;

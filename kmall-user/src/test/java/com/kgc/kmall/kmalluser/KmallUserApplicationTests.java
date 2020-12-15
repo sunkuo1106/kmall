040731b3c46108728bec1e2735aab3c1f8b686e8
sunkuo1106 <sunkuo1106@163.com>
2020-12-15 10:27:37 +0800

    提交kmall-user

---
+++ kmall-user/src/test/java/com/kgc/kmall/kmalluser/KmallUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgc.kmall.kmalluser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmallUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

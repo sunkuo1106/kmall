dd7c2c0d5973c89c58599f360aa9854abb904379
sunkuo1106 <sunkuo1106@163.com>
2021-01-03 16:29:57 +0800

    使用缓存优化商品详情

--- kmall-manager-service/src/test/java/com/kgc/kmall/manager/KmallManagerServiceApplicationTests.java
+++ kmall-manager-service/src/test/java/com/kgc/kmall/manager/KmallManagerServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.kgc.kmall.manager;
 
 import com.kgc.kmall.bean.PmsBaseCatalog1;
+import com.kgc.kmall.manager.utils.RedisUtil;
 import com.kgc.kmall.service.CatalogService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import redis.clients.jedis.Jedis;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -14,11 +16,17 @@ class KmallManagerServiceApplicationTests {
     @Resource
     CatalogService catalogService;
 
+    @Resource
+    RedisUtil redisUtil;
+
     @Test
     void contextLoads() {
-        List<PmsBaseCatalog1> catalog1 = catalogService.getCatalog1();
-        for (PmsBaseCatalog1 pmsBaseCatalog1 : catalog1) {
-            System.out.println(pmsBaseCatalog1);
+        try {
+            Jedis jedis = redisUtil.getJedis();
+            String k1 = jedis.get("name");
+            System.out.println(k1);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 

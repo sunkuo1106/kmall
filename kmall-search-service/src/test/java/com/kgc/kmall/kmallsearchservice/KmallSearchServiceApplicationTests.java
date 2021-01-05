7eb3fd230217c18c0a9039d3390546a1d8a73787
sunkuo1106 <sunkuo1106@163.com>
2021-01-05 19:02:56 +0800

    增加ES高亮查询

--- kmall-search-service/src/test/java/com/kgc/kmall/kmallsearchservice/KmallSearchServiceApplicationTests.java
+++ kmall-search-service/src/test/java/com/kgc/kmall/kmallsearchservice/KmallSearchServiceApplicationTests.java
@@ -5,11 +5,21 @@ import com.kgc.kmall.bean.PmsSkuInfo;
 import com.kgc.kmall.service.SkuService;
 import io.searchbox.client.JestClient;
 import io.searchbox.core.Index;
+import io.searchbox.core.Search;
 import org.apache.dubbo.config.annotation.Reference;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeanUtils;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
+
 import javax.annotation.Resource;
+import javax.naming.directory.SearchResult;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +32,9 @@ class KmallSearchServiceApplicationTests {
 
     @Resource
     JestClient jestClient;
+
+    @Resource
+    ElasticsearchRestTemplate elasticsearchRestTemplate;
 
     @Test
     void contextLoads() {
@@ -47,6 +60,75 @@ class KmallSearchServiceApplicationTests {
     public void test1(){
         List<PmsSkuInfo> allSku = skuService.getAllSku();
         System.out.println(allSku.size());
+    }
+
+    @Test
+    void testSearch(){
+        String json="\n" +
+                "{\n" +
+                "  \"query\": {\n" +
+                "    \"bool\": {\n" +
+                "      \"filter\": [\n" +
+                "          {\"terms\":{\"skuAttrValueList.valueId\":[\"39\",\"40\",\"41\",\"42\"]}},\n" +
+                "          {\"term\":{\"skuAttrValueList.valueId\":\"43\"}}\n" +
+                "        ], \n" +
+                "      \"must\": \n" +
+                "        {\n" +
+                "          \"match\": {\n" +
+                "            \"skuName\": \"iphone\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      \n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        Search search=new Search.Builder(json).addIndex("kmall").addType("PmsSkuInfo").build();
+        try {
+            io.searchbox.core.SearchResult searchResult = jestClient.execute(search);
+            List<io.searchbox.core.SearchResult.Hit<PmsSearchSkuInfo, Void>> hits = searchResult.getHits(PmsSearchSkuInfo.class);
+            for (io.searchbox.core.SearchResult.Hit<PmsSearchSkuInfo, Void> hit : hits) {
+                PmsSearchSkuInfo skuInfo = hit.source;
+                System.out.println(skuInfo.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSearchBuilder(){
+        BoolQueryBuilder boolQueryBuilder=new BoolQueryBuilder();
+        TermQueryBuilder termQueryBuilder=new TermQueryBuilder("skuAttrValueList.valueId","39");
+        TermQueryBuilder termQueryBuilder2=new TermQueryBuilder("skuAttrValueList.valueId","43");
+
+        boolQueryBuilder.filter(termQueryBuilder);
+        boolQueryBuilder.filter(termQueryBuilder2);
+
+        MatchQueryBuilder matchQueryBuilder=new MatchQueryBuilder("skuName","iphone");
+
+        boolQueryBuilder.must(matchQueryBuilder);
+
+        SearchSourceBuilder searchSourceBuilder=new SearchSourceBuilder();
+        searchSourceBuilder.query(boolQueryBuilder);
+
+        System.out.println(searchSourceBuilder.toString());
+    }
+
+    @Test
+    public void testSearchBuilder2(){
+        BoolQueryBuilder boolQueryBuilder=new BoolQueryBuilder();
+        boolQueryBuilder.must(new MatchQueryBuilder("skuName","iphone"));
+
+        SearchQuery searchQuery=new NativeSearchQueryBuilder()
+                .withQuery(boolQueryBuilder)
+                .build();
+
+        List<PmsSearchSkuInfo> pmsSearchSkuInfos = elasticsearchRestTemplate.queryForList(searchQuery, PmsSearchSkuInfo.class);
+        for (PmsSearchSkuInfo pmsSearchSkuInfo : pmsSearchSkuInfos) {
+            System.out.println(pmsSearchSkuInfo.toString());
+        }
+
+
     }
 
 }

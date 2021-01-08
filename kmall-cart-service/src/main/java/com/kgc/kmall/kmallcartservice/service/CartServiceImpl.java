fdef9b6120cd17fb34d1083b276aa4707b74ed36
sunkuo1106 <sunkuo1106@163.com>
2021-01-08 22:17:36 +0800

    增加购物车页面、实现未登录状态添加商品到cookie、登录状态添加数据库

---
+++ kmall-cart-service/src/main/java/com/kgc/kmall/kmallcartservice/service/CartServiceImpl.java
@@ -0,0 +1,110 @@
+package com.kgc.kmall.kmallcartservice.service;
+
+import com.alibaba.fastjson.JSON;
+import com.kgc.kmall.bean.OmsCartItem;
+import com.kgc.kmall.bean.OmsCartItemExample;
+import com.kgc.kmall.kmallcartservice.mapper.OmsCartItemMapper;
+import com.kgc.kmall.service.CartService;
+import com.kgc.kmall.utils.RedisUtil;
+import org.apache.dubbo.config.annotation.Service;
+import org.redisson.api.RedissonClient;
+import org.redisson.misc.Hash;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+
+@Service
+@Component
+public class CartServiceImpl implements CartService {
+
+    @Resource
+    OmsCartItemMapper omsCartItemMapper;
+
+    @Resource
+    RedisUtil redisUtil;
+
+    @Resource
+    RedissonClient redissonClient;
+
+    @Override
+    public OmsCartItem ifCartExistByUser(String memberId, long skuId) {
+        OmsCartItemExample example=new OmsCartItemExample();
+        OmsCartItemExample.Criteria criteria = example.createCriteria();
+        criteria.andMemberIdEqualTo(Long.parseLong(memberId));
+        criteria.andProductSkuIdEqualTo(skuId);
+        List<OmsCartItem> omsCartItems = omsCartItemMapper.selectByExample(example);
+        if (omsCartItems!=null&&omsCartItems.size()>0){
+            return omsCartItems.get(0);
+        }else {
+            return null;
+        }
+    }
+
+    @Override
+    public void addCart(OmsCartItem omsCartItem) {
+        omsCartItemMapper.insertSelective(omsCartItem);
+    }
+
+    @Override
+    public void updateCart(OmsCartItem omsCartItemFromDb) {
+        omsCartItemMapper.updateByPrimaryKeySelective(omsCartItemFromDb);
+    }
+
+    @Override
+    public void flushCartCache(String memberId) {
+        OmsCartItemExample example=new OmsCartItemExample();
+        OmsCartItemExample.Criteria criteria = example.createCriteria();
+        criteria.andMemberIdEqualTo(Long.parseLong(memberId));
+        List<OmsCartItem> omsCartItems = omsCartItemMapper.selectByExample(example);
+
+        //同步到redis缓存中
+        Jedis jedis = redisUtil.getJedis();
+        Map<String,String> map=new HashMap<>();
+        for (OmsCartItem omsCartItem : omsCartItems) {
+            map.put(omsCartItem.getProductSkuId().toString(), JSON.toJSONString(omsCartItem));
+        }
+        jedis.del("user:"+memberId+":cart");
+        jedis.hmset("user:"+memberId+":cart",map);
+
+        jedis.close();
+    }
+
+    @Override
+    public List<OmsCartItem> cartList(String memberId) {
+        Jedis jedis=null;
+        //Lock lock=null;
+        List<OmsCartItem> omsCartItems=new ArrayList<>();
+        try {
+            jedis = redisUtil.getJedis();
+            List<String> hvals  = jedis.hvals("user:" + memberId + ":cart");
+            if(hvals!=null){
+                for (String hval : hvals) {
+                    OmsCartItem omsCartItem = JSON.parseObject(hval, OmsCartItem.class);
+                    omsCartItems.add(omsCartItem);
+                }
+            }else{
+                //lock = redissonClient.getLock("Omslock");//生命锁
+                //lock.lock();
+                OmsCartItemExample example=new OmsCartItemExample();
+                example.createCriteria().andMemberIdEqualTo(Long.parseLong(memberId));
+                omsCartItems = omsCartItemMapper.selectByExample(example);
+            }
+        } catch (Exception e) {
+            // 处理异常，记录系统日志
+            e.printStackTrace();
+            //String message = e.getMessage();
+            //logService.addErrLog(message);
+            return null;
+        }finally {
+            //lock.unlock();
+            jedis.close();
+        }
+        return omsCartItems;
+    }
+}
